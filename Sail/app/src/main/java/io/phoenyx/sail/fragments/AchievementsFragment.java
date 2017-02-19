87616b1a646a37877dbc48cb0a5e977aa905ed74
Terrance Li <terranceli.terbear@gmail.com>
2017-02-18 18:37:18 -0800

    Initialized Fragments

--- Sail/app/src/main/java/io/phoenyx/sail/fragments/AchievementsFragment.java
+++ Sail/app/src/main/java/io/phoenyx/sail/fragments/AchievementsFragment.java
@@ -1,8 +1,19 @@
 package io.phoenyx.sail.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
 /**
  * Created by terrance on 2/18/17.
  */
 
-public class AchievementsFragment {
+public class AchievementsFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+
+
+    }
 }
--- Sail/app/src/main/java/io/phoenyx/sail/fragments/PromisesFragment.java
+++ Sail/app/src/main/java/io/phoenyx/sail/fragments/PromisesFragment.java
@@ -1,8 +1,20 @@
 package io.phoenyx.sail.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
 /**
  * Created by terrance on 2/18/17.
  */
 
-public class PromisesFragment {
+public class PromisesFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+
+
+    }
 }
