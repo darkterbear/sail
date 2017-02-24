fa89bd7190a579d2bff36203ad733a9c65593117
DarkTerbear <terranceli.terbear@gmail.com>
2017-02-24 15:18:59 -0800

    Workin on shit

--- Sail/app/src/main/java/io/phoenyx/sail/GoalsAdapter.java
+++ Sail/app/src/main/java/io/phoenyx/sail/GoalsAdapter.java
@@ -90,9 +90,9 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsAdapter.GoalsViewHol
                 Achievement achievement = new Achievement(goal.getTitle(), goal.getDescription(), date, goal.isStarred());
                 dbHandler.createAchievement(achievement);
                 dbHandler.deleteGoal(goal.getId());
-                goals.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, getItemCount() - position);
+                goals.remove(holder.getAdapterPosition());
+                notifyItemRemoved(holder.getAdapterPosition());
+                notifyItemRangeChanged(holder.getAdapterPosition(), getItemCount() - holder.getAdapterPosition());
             }
         });
 
